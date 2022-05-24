e5604d7ede12239b17a6881327ab6a1b46b56db0
Patricia Saboya <105935639+PatriciaSaboya@users.noreply.github.com>
2022-05-24 09:40:09 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,9 @@ import streamlit
 streamlit.title('My Parents New Healthy Dinner')
 
 streamlit.header('Breakfast Favorites')
-streamlit.text(🥣'Omega 3 & Blueberry Oatmeal')
+streamlit.text('\N{U+1F963}Omega 3 & Blueberry Oatmeal')
 streamlit.text(🥗'Kale, Spinach & Rocket Smoothie')
 streamlit.text(🐔'Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocato Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
